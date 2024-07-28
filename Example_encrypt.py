91f62baf72055200edd567c6028c3925197be143
L0ng-nguY3n-470115 <longnguyen511074@gmail.com>
2024-07-28 19:42:50 +0700

    Create Example_encrypt.py

---
+++ Example_encrypt.py
@@ -0,0 +1,18 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+
+def decrypt_aes_cbc(ciphertext, key):
+    # Tách IV ra từ bản mã
+    iv = ciphertext[:16] # 4b4353434b4353434b4353434b435343
+    ciphertext = ciphertext[16:]
+    
+    # Tạo đối tượng AES
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    
+    # Giải mã và trả về bản rõ
+    plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    return plaintext.decode()
+
+# Ví dụ sử dụng
+decrypted_text = decrypt_aes_cbc(ciphertext, key)
+print(decrypted_text)
